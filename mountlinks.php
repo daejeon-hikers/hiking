3273465a8acca92b9d9813f8de2b52f36ec0c2c2
pablo3620 <47928304+pablo3620@users.noreply.github.com>
2020-09-14 18:40:13 +1200

    test

---
+++ mountlinks.php
@@ -0,0 +1,24 @@
+<?php
+echo '<a href="introductionMountains.html">Introduction to Mountains
+</a><a href="OuSeongEeSan.html">OuSeongEeSan
+</a><a href="ManInSan.html">ManInSan
+</a><a href="GyerYongSan.html">GyerYongSan
+</a><br>
+<a href="BoMunSan.html">BoMunSan
+</a><a href="KaJeongSan.html">KaJeongSan
+</a><a href="GapHaSan.html">GapHaSan
+</a><a href="CheongNamDae.html">CheongNamDae
+</a><br>
+<a href="GuBongSan.html">GuBongSan
+</a><a href="CheonTaeSan.html">CheonTaeSan
+</a><a href="JilHyeonSan.html">JilHyeonSan
+</a><a href="NaBalBong.html">NaBalBong
+</a><br>
+<a href="SongNiSan.html">SongNiSan
+</a><a href="YoungSeongSan.html">YoungSeongSan
+</a><a href="DaeDunSan.html">DaeDunSan
+</a><a href="MaEeSan.html">MaEeSan
+</a><br>
+<a href="UnJuSanSeong.html">UnJuSanSeong
+</a><a href="DeoGyuSan.html">DeoGyuSan
+</a>';?>
